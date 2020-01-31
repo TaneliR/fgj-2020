80bb4625fd1ce9eb053dde4b9baa442dc9020f0a
Taneli Riihimäki <taneli.riihimaki@liveto.io>
2020-01-31 20:05:38 +0200

    Player controller WIP

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 500f;
+    public Rigidbody2D rb;
+    public Rigidbody2D rightTela;
+    public Rigidbody2D leftTela;
+
+    float movementRight;
+    float movementLeft;
+    // Update is called once per frame
+    void Update()
+    {
+        movementRight = Input.GetAxisRaw("Horizontal");
+        movementLeft = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate() {
+        rightTela.AddForce(transform.up * movementRight * moveSpeed * Time.fixedDeltaTime);
+        leftTela.AddForce(transform.up * movementLeft * moveSpeed * Time.fixedDeltaTime);
+    }
+}
