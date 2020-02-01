89510a81c179ebe8dcb133e249c806db21acd9f4
Arttu Rousku <M1484@student.jamk.fi>
2020-02-01 16:08:07 +0200

    Scripts for detection check added

---
+++ Assets/ConeCollisionCheck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConeCollisionCheck : MonoBehaviour
+{
+    public EnemyFov fov;
+
+    public LineRenderer lineOfSight;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Turpaan!");
+            fov.LineCheck();
+        }
+
+    }
+
+}
