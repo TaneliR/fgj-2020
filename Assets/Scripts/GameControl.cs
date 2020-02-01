9cb150f4cb8cae15b4f45d37640966d6b1619f94
Taneli Riihimäki <taneli.riihimaki@liveto.io>
2020-02-01 19:54:49 +0200

    Open Doors

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -8,12 +8,16 @@ public class GameControl : MonoBehaviour
     private int score = 0;
     public Text scoreText;
     public Image oilBar;
+    public OpenDoor door;
     // Start is called before the first frame update
 
     public void IncreaseScore(int increase)
     {
         score += increase;
-        scoreText.text = "Score: " + score;
+        scoreText.text = "Scraps: " + score;
+        if (score == 1) {
+            door.OpenDoors();
+        }
     }
     public void SetOilBar(float amount) {
         oilBar.fillAmount = amount / 100f;
