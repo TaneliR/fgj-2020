78aeae7340cba0e63779bcd1634d6d0b63973b90
Taneli Riihimäki <taneli.riihimaki@liveto.io>
2020-02-01 20:16:49 +0200

    Screenshake when doors open

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -8,15 +8,30 @@ public class GameControl : MonoBehaviour
     private int score = 0;
     public Text scoreText;
     public Image oilBar;
-    public OpenDoor door;
+    public OpenDoor firstDoors;
+    public OpenDoor secondDoors;
+    public OpenDoor thirdDoors;
+    public ShakeBehavior camera;
+    public int firstDoorsAmount = 4;
+    public int secondDoorsAmount = 8;
+    public int thirdDoorsAmount = 12;
     // Start is called before the first frame update
 
     public void IncreaseScore(int increase)
     {
         score += increase;
         scoreText.text = "Scraps: " + score;
-        if (score == 1) {
-            door.OpenDoors();
+        if (score == firstDoorsAmount) {
+            camera.TriggerShake();
+            firstDoors.OpenDoors();
+        }
+        if (score == secondDoorsAmount) {
+            camera.TriggerShake();
+            secondDoors.OpenDoors();
+        }
+        if (score == thirdDoorsAmount) {
+            camera.TriggerShake();
+            thirdDoors.OpenDoors();
         }
     }
     public void SetOilBar(float amount) {
