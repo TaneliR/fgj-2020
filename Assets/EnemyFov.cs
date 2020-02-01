4229530291bc45de72f5cbdbfe379d9c65509167
Arttu Rousku <M1484@student.jamk.fi>
2020-02-01 09:46:42 +0200

    EnemyFov multiple linerender testing

--- Assets/EnemyFov.cs
+++ Assets/EnemyFov.cs
@@ -7,7 +7,9 @@ public class EnemyFov : MonoBehaviour
     public float rotationSpeed;
     public float distance;
 
-    public LineRenderer lineOfSight;
+    public LineRenderer lineOfSightCenter;
+    public LineRenderer lineOfSightLeft;
+
 
     void Start(){
         Physics2D.queriesStartInColliders = false;
@@ -18,20 +20,23 @@ public class EnemyFov : MonoBehaviour
         transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
 
         RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, transform.right, distance);
-        if(hitInfo.collider != null){
-            Debug.DrawLine(transform.position, hitInfo.point, Color.red);
-            lineOfSight.SetPosition(1, hitInfo.point);
+        RaycastHit2D hitInfoLeft = Physics2D.Raycast(transform.position, transform.right, distance);
+
+        if (hitInfo.collider != null){
+            lineOfSightCenter.SetPosition(1, hitInfo.point);
+            lineOfSightLeft.SetPosition(2, hitInfoLeft.point);
             
             if(hitInfo.collider.CompareTag("Player")){
                 Destroy(hitInfo.collider.gameObject);
             }
         } else {
-            Debug.DrawLine(transform.position, transform.position + transform.right * distance, Color.green);
-            lineOfSight.SetPosition(1, transform.position + transform.right * distance);
+            lineOfSightCenter.SetPosition(1, transform.position + transform.right * distance);
+            lineOfSightLeft.SetPosition(2, transform.position + transform.right * distance);
         }
         
 
-        lineOfSight.SetPosition(0, transform.position);
+        lineOfSightCenter.SetPosition(0, transform.position);
+        lineOfSightLeft.SetPosition(0, transform.position);
 
     }
 }
