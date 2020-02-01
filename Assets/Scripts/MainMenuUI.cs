0bd6e7cd72b64c90620efc55a5ab5834438b6de3
Taneli Riihimäki <taneli.riihimaki@liveto.io>
2020-02-01 12:33:40 +0200

    Simple scene management WIP

---
+++ Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public Button startButton;
+    private void Awake() {
+        startButton.GetComponent<Button>().onClick.AddListener(StartGameClick);
+    }
+
+    void StartGameClick() {    
+        Debug.Log("Click on start game");
+        Loader.Load(Loader.Scene.GameScene);
+    }
+}
